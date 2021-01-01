62683dae4dd8943efb6e7bafc77163286c9f4729
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-31 18:05:16 -0700

    starting over

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/resume" component={Resume} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
