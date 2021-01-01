3669645dba90f39a35b677c46d8fe59ff9073047
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-31 21:21:07 -0700

    adding in about page content

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "./style.css";
 
-function Resume() {
+
+
+function SabineResume() {
     return(
-    <div class="container" id ="maincontent">
-    <h1 class="homepagename">Resume</h1>
-    <h2 class= "sabinebio" style="display : inline-block">Connect with Sabine on LinkedIn : </h2>
-    <a href="https://www.linkedin.com/in/sabinehutter/" style="display : inline-block; padding-top: 13px;" class="fa fa-linkedin-square"></a>
+    <div className="container" id ="maincontent">
+    <h1 className="homepagename">Resume</h1>
+    <h2 className= "sabinebio" style={{display : "inline-block"}}>Connect with Sabine on LinkedIn : </h2>
+    {/* <a href="https://www.linkedin.com/in/sabinehutter/" style="display : inline-block; padding-top: 13px;" className="fa fa-linkedin-square"></a> */}
 
-    <iframe src="/Assets/SabineHutterResume.pdf" width="100%" height="1100px">
-    </iframe>
+    <iframe src="../../Assets/SabineHutterResume.pdf" title="title">
+     Presss me: <a href="../../Assets/SabineHutterResume.pdf">Download PDF</a>
+</iframe>
     
 </div>
     )
 }
-export default Resume
+export default SabineResume
