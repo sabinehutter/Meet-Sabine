2699bfc8819f0cd7b6e228631c33b24aee9fe199
Sabine-Hutter <Sabine.Hutter@ibm.com>
2021-01-02 15:05:59 -0700

    adding project json

--- src/components/AboutContainer/index.js
+++ src/components/AboutContainer/index.js
@@ -15,6 +15,7 @@ function AboutSabine(props) {
             <p className="sabinebio1"> Sabine works as a Senior Data Scientist and Consultant for IBM in Chicago, IL. Sabine has a Bachelors of Science in Information Analytics from the University of Michigan, Certificate of Sales and Marketing from the Ross School of Business - University of Michigan, and is currently pursuing a certificate from Columbia Engineering in Full-Stack Development. Sabine enjoys traveling, cooking, learning new things, and binge watching tv (specifically Netflix). Front-end development is Sabine's passion, she enjoys working in Vue, Javascript, React, Angular, and helping design interactive ui's for clients. This website highlights Sabine's accompliments, and showcases her most recent projects! Look around and get to know Sabine, and make sure to connect :)  </p>
 
             <h1 className="homepagename">Let's Connect!</h1>
+            <div>
             <a href="https://www.facebook.com/huttersa"  className = "fa">
             <FontAwesomeIcon icon={faFacebook} />
             </a>
@@ -42,7 +43,7 @@ function AboutSabine(props) {
             <a href="mailto:SabineAHutter@gmail.com" className = "fa">
             <FontAwesomeIcon icon={faEnvelope} />
             </a>
-
+            </div>
         </div>
     );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,8 @@ import AboutSabine from "../components/AboutContainer"
 import BackgroundComponent from "../components/Background"
 import Background from "../Assets/background.jpg"
 import Image from "../Assets/sabine.jpg"
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { apple } from '@fortawesome/free-brands-svg-icons'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-function About(props) {
+function About() {
   return (
     <div>
     <BackgroundComponent backgroundImage = {Background} style = {{height : "2000px"}}></BackgroundComponent>
