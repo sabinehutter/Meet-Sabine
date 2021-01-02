aac741b244100dbac380201fbb1e3d8c8b975cf1
Sabine-Hutter <Sabine.Hutter@ibm.com>
2021-01-02 14:07:24 -0700

    finishing about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,15 @@ import React from "react";
 import AboutSabine from "../components/AboutContainer"
 import BackgroundComponent from "../components/Background"
 import Background from "../Assets/background.jpg"
+import Image from "../Assets/sabine.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
-function About() {
+function About(props) {
   return (
     <div>
     <BackgroundComponent backgroundImage = {Background} style = {{height : "2000px"}}></BackgroundComponent>
-    <AboutSabine/>
+    <AboutSabine backgroundImage = {Image}/>
+    <FontAwesomeIcon icon={['fab', 'apple']} />
     </div>
   );
 }
